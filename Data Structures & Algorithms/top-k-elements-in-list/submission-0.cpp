16539963b797301d3909d1a486f3061bc89aab11
kushagra bhargava <142936935+Kushagra651@users.noreply.github.com>
2026-08-31 16:17:21 +0530

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>mpp;
-        for(auto i : nums){
-            mpp[i]++;
-        }
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>
-        > pq;
+        for(auto i: nums)mpp[i]++;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         for(auto i: mpp){
-            pq.push({i.second,i.first});
-            if(pq.size()>k)pq.pop();
+            int num = i.first;
+            int freq = i.second;
+            if(pq.size()<k){
+                pq.push({freq,num});
+            }
+            else if(freq>pq.top().first){
+                pq.pop();
+                pq.push({freq,num});
+            }
         }
         vector<int> ans;
         while(!pq.empty()){
@@ -20,5 +21,5 @@ public:
             pq.pop();
         }
         return ans;
-    }
+        }
 };
